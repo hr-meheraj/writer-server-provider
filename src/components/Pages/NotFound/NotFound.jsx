164547aedaffb4fac6cb25d74a5b864eb323be66
hrmeheraj2021 <hrmeheraj2021@gmail.com>
2022-04-17 13:47:49 -0400

    Not Found Page Done!

--- src/components/Pages/NotFound/NotFound.jsx
+++ src/components/Pages/NotFound/NotFound.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import notFound from './notFound.png'
+import {Link} from 'react-router-dom'
 function NotFound() {
     return (
-        <div>
-            <h3>404 Not Found</h3> 
+        <div className='flex justify-center items-center '>
+           <div>
+               <img src={notFound} alt="Not Found" className='w-[300px] md:w-[380px] mg:-w-[430px] py-4'/>
+                <Link className='text-center p-2 block hover:text-blue-900 font-semibold text=[22px]' to='/'>Back to the Home &larr; </Link>
+           </div>
         </div>
     )
 }
